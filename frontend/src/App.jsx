6780f995c2b3cec914d8573469f11a99802af901
Abdullah-NI <abdullahnidbd786@gmail.com>
2026-03-02 15:48:39 +0530

    nave file added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,15 @@ import Pharma4OPage from './components/landing_page/pharma4O/pharma4Opage';
 import ServicePage from './components/landing_page/service/servicePage';
 import AboutPage from './components/landing_page/about/aboutPage';
 import ContactPage from './components/landing_page/contact/contactPage';
-import DiabetesCare from './components/landing_page/HealthScore/HealthScore';
+import HealthScore from './components/landing_page/HealthScore/HealthScore';
 import ScrollToTop from './components/landing_page/ScrollToTop';
+import Nutrition from './components/landing_page/NavPages/Nutrition';
+import DiabetesCare from './components/landing_page/NavPages/DiabetesCare';
+import WomenCare from './components/landing_page/NavPages/WomenCare';
+import HeartCare from './components/landing_page/NavPages/HeartCare';
+import DietPlan from './components/landing_page/NavPages/DietPlan';
+import CareCoordinators from './components/landing_page/NavPages/CareCoordinators';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { useState } from "react";
@@ -29,7 +36,14 @@ export default function App() {
           <Route path='/about' element={<AboutPage/>}/>
           <Route path='/contact' element={<ContactPage/>}/>
           <Route path='/commingsoon' element={<CommingSoon/>}/>
-          <Route path='/health-score' element={<DiabetesCare/>}/>
+          <Route path='/health-score' element={<HealthScore/>}/>
+
+          <Route path='/nutirition' element={<Nutrition/>}/>
+          <Route path='/diabetescare' element={<DiabetesCare/>}/>
+          <Route path='/womencare' element={<WomenCare/>}/>
+          <Route path='/heartcare' element={<HeartCare/>}/>
+          <Route path='/dietplan' element={<DietPlan/>}/>
+          <Route path='/carecoordinators' element={<CareCoordinators/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
